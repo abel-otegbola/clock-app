c4401ae9ce97e72614510c8adf91b5b0ca0171be
Akshit <akshitarora9211@gmail.com>
2022-10-16 14:07:20 +0530

    undo semicolon changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
 // Clock functionality
 
-let hr = document.querySelector("section .hr")
-let min = document.querySelector("section .min")
-let sec = document.querySelector("section #sec")
+let hr = document.querySelector("section .hr");
+let min = document.querySelector("section .min");
+let sec = document.querySelector("section #sec");
 
 setInterval(() => {
-    hr.style.transform = `rotateZ(${new Date().getHours() * 30}deg)`
-    min.style.transform = `rotateZ(${new Date().getMinutes() * 6}deg)`
-    sec.style.transform = `rotateZ(${new Date().getSeconds() * 6}deg)`
-}, 1000)
+    hr.style.transform = `rotateZ(${new Date().getHours() * 30}deg)`;
+    min.style.transform = `rotateZ(${new Date().getMinutes() * 6}deg)`;
+    sec.style.transform = `rotateZ(${new Date().getSeconds() * 6}deg)`;
+}, 1000);
 
 //check if mobile device
 function isMobileDevice() {
@@ -17,85 +17,85 @@ function isMobileDevice() {
             navigator.userAgent || window.innerWidth <= 500
         )
     ) {
-        return true
+        return true;
     } else {
-        return false
+        return false;
     }
 }
 
 // Mobile menu functionality
 function toggleMenu() {
-    document.querySelector("aside").classList.toggle("active")
+    document.querySelector("aside").classList.toggle("active");
 }
 
 // Classlist changes selector helpers
 const remove = (el, option) => {
-    el.classList.remove(option)
-}
+    el.classList.remove(option);
+};
 const add = (el, option) => {
-    el.classList.add(option)
-}
+    el.classList.add(option);
+};
 
 // Color theme functionality
-let colormode = localStorage.getItem("colormode")
-let colorButtons = document.querySelectorAll("section aside ul.modes li")
+let colormode = localStorage.getItem("colormode");
+let colorButtons = document.querySelectorAll("section aside ul.modes li");
 
 for (let i = 0; i < colorButtons.length; i++) {
     colorButtons[i].addEventListener("click", () => {
         if (i === 0) {
-            remove(colorButtons[1], "active")
-            localStorage.setItem("colormode", "light")
-            remove(document.body, "dark")
+            remove(colorButtons[1], "active");
+            localStorage.setItem("colormode", "light");
+            remove(document.body, "dark");
         } else {
-            remove(colorButtons[0], "active")
-            localStorage.setItem("colormode", "dark")
-            add(document.body, "dark")
+            remove(colorButtons[0], "active");
+            localStorage.setItem("colormode", "dark");
+            add(document.body, "dark");
         }
         if (isMobileDevice()) {
-            toggleMenu()
+            toggleMenu();
         }
-        colorButtons[i].classList.add("active")
-    })
+        colorButtons[i].classList.add("active");
+    });
 }
 if (colormode === "dark") {
-    add(document.body, "dark")
-    remove(colorButtons[0], "active")
-    add(colorButtons[1], "active")
+    add(document.body, "dark");
+    remove(colorButtons[0], "active");
+    add(colorButtons[1], "active");
 }
 
 // Themes functionality
-let theme = localStorage.getItem("theme")
-let themeButtons = document.querySelectorAll("section aside ul.themes li")
-let themeContainer = document.querySelector("section article")
+let theme = localStorage.getItem("theme");
+let themeButtons = document.querySelectorAll("section aside ul.themes li");
+let themeContainer = document.querySelector("section article");
 
 for (let i = 0; i < themeButtons.length; i++) {
     themeButtons[i].addEventListener("click", () => {
         for (let j = 0; j < themeButtons.length; j++) {
-            remove(themeButtons[j], "active")
+            remove(themeButtons[j], "active");
         }
-        add(themeButtons[i], "active")
+        add(themeButtons[i], "active");
         themeContainer.setAttribute(
             "data-theme",
             themeButtons[i].getAttribute("data-theme")
-        )
+        );
         if (isMobileDevice()) {
-            toggleMenu()
+            toggleMenu();
         }
         localStorage.setItem(
             "theme",
             themeButtons[i].getAttribute("data-theme")
-        )
-    })
+        );
+    });
 }
 // Get the skin when app loads
-themeContainer.setAttribute("data-theme", theme)
+themeContainer.setAttribute("data-theme", theme);
 for (let i = 0; i < themeButtons.length; i++) {
-    remove(themeButtons[i], "active")
+    remove(themeButtons[i], "active");
     if (themeButtons[i].getAttribute("data-theme") === theme) {
-        add(themeButtons[i], "active")
+        add(themeButtons[i], "active");
     }
 }
 
 // Footer year
 document.querySelector("footer .footer-year").textContent =
-    new Date().getFullYear()
+    new Date().getFullYear();
